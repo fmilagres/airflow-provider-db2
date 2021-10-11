971ae027f27abe5fe3b98f0eb85a4c90c2833ea4
FERNANDA COLONEZI MILAGRES <Fernanda.MILAGRES@ibm.com>
2021-10-11 14:17:46 -0300

    '-' not supported

--- setup.py
+++ setup.py
@@ -5,23 +5,23 @@ from setuptools import find_packages, setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-"""Perform the package airflow-provider-DB2-custom setup."""
+"""Perform the package airflow_provider_db2 setup."""
 setup(
-    name='airflow-provider-db2',
+    name='airflow_provider_db2',
     version="0.0.1",
     description='A provider package built by AIOPS IBM team',
     long_description=long_description,
     long_description_content_type='text/markdown',
     entry_points={
     "apache_airflow_provider": [
-        "provider_info=airflow-provider-db2.__init__:get_provider_info"
+        "provider_info=airflow_provider_db2.__init__:get_provider_info"
           ]
       },
     license='Apache License 2.0',
-    packages=['airflow-provider-db2', 'airflow-provider-db2.hooks'],
+    packages=['airflow_provider_db2', 'airflow_provider_db2.hooks'],
     install_requires=['apache-airflow>=2.0'],
     setup_requires=['setuptools', 'wheel'],
     author='Fernanda Milagres',
-    author_email='fernanda.milagres@ibm.com',
+    author_email='fernandamilagres@gmail.com',
     python_requires='~=3.7',
 )
